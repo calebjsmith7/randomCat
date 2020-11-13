eb0e0df7887296c7465e14f0dbbeb99d00cf84d3
Caleb Smith <68673831+calebjsmith7@users.noreply.github.com>
2020-11-13 12:31:43 -0700

    Update App.js
    cleaned up code and redundancies

--- src/App.js
+++ src/App.js
@@ -3,8 +3,7 @@ import './App.css';
 import React, {Component} from 'react';
 
 
-class App extends
-Component {
+class App extends Component {
 
     state = {
       loading: true,
@@ -15,12 +14,11 @@ Component {
     async componentDidMount(){
       const url = "https://aws.random.cat/meow";
       const response = await fetch(url);
-      const data = await response.json();
+      const data = response.json();
       this.setState({
         loading: false,
         cat: data.file
       });
-
     }
 
   render(){
